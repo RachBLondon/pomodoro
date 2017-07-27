20a164615b206ddf05d27d196e1c36e5b9ca9f94
Rachel Black <rachelblack@Rachels-MacBook-Pro.local>
2017-07-27 07:54:37 +0100

    setInterval working

--- src/components/timer.js
+++ src/components/timer.js
@@ -6,26 +6,42 @@ const limit = 15
 let counter = 0
 let recoder
 
-export function getTimeElapsed(startTime){
-    const currentTime = new Date()
-    return currentTime - startTime
-}
+//issue with chrome 
+// fix with Date.now (maybe do to string)
+// https://bugs.chromium.org/p/chromium/issues/detail?id=161319
+
+
 
 export default class Timer extends Component{
-    constructor(props) {
-        super(props)
-        this.state = {startTime : null}
-        this.startTimer = this.startTimer.bind(this)
+    
+    constructor(props){
+        super(props);
+            this.state = {startTime: Date.now(), test: "hello"}
     }
 
-    componentWillMount(){
-        chrome.storage.sync.get('startTime', (startTime)=>{
-            console.log("chrome get ", startTime)
-            this.setState(startTime)
-        })
+    startTimer(){
+        console.log('in start Timer this ', this)
+        this.timer = setInterval(()=>{
+           this.tick()
+        }, 1000)
     }
 
-    //TODO save all state in chrome storage on component will unmount
+    stopTimer(){
+        console.log("stop timer  this", this)
+        clearInterval(this.time)
+    }
+
+    tick(){
+        console.log("tick", this.state, "this", this)
+        if(this.state.startTime){
+            this.setState({timeElapsed : Date.now() - this.state.startTime })
+
+        }
+    }
+
+  
+
+   // TODO save all state in chrome storage on component will unmount
     // componentWillUnmount(){
     //     console.log("in componne will unmount " )
     //     chrome.storage.sync.set({'startTime': this.state.startTime}, function(){
@@ -33,24 +49,52 @@ export default class Timer extends Component{
     //     })
     // }
 
-    startTimer(){
-        const startTime = new Date()
-        this.setState({startTime : startTime})
-        return startTime
-    }
+    // startTimer(){
+    //     const startTime = Date.now()
+    //     this.setState({startTime :startTime })
+    // }
 
-    getStartTime(){
-        if(this.state.startTime){
-            console.log("in if getStartTime ", this.state)
-            return this.state.startTime.getSeconds()
-        }
-    }
+
+   
+
+    // saveInChrome(){
+    //     console.log("in save in chrome ", this.state.startTime)
+    //     chrome.storage.sync.set({startTime: this.state.startTime}, function(){
+    //     })
+    // }
+
+
+    // getTimeElapsed(){
+    //     if(this.state.startTime){
+    //         setInterval(function(){
+    //             this.setState({timeElapsed: Date.now() - this.state.startTime })
+    //         },1000)
+    //     }
+    // }
+
+    
+
+    // getChromeTime(){
+    //       chrome.storage.sync.get("startTime", (startTime)=>{
+    //         console.log("chrome get ", startTime)
+    //     })
+    // }
+
+    
 
     render(){
+        console.log("render this. state", this.state, "this ", this)
         return <div style={{height : '100px'}}>
-                    <button onClick={this.startTimer}> button</button>
-                    <span className="startTime">{this.getStartTime()}</span>
+                    <span className="startTime">State Start Time{this.state.startTime}</span>
+                    <span className="elapsedTime">Elapsed Time {this.state.timeElapsed}</span>
+                    <button onClick={this.startTimer.bind(this)}> button</button>
+                    <button onClick={this.stopTimer}>Stop </button>
+                    
+                    
+
                 </div>
     }
 }
+                    // <button onClick={this.saveInChrome.bind(this)}> Save in chrome storage</button>
+                    // <button onClick={this.getChromeTime}> Save in chrome storage</button>
 
--- test/components/timer.spec.js
+++ test/components/timer.spec.js
@@ -18,59 +18,55 @@ describe('Timer', ()=>{
         expect(Timer).to.exist
     })
 
-    describe('loading state & storage', ()=>{
-        before(function () {
-            global.chrome = chrome
-        })
-        it('should have inital state of startTime = null', ()=>{
-            const wrapper = shallow(<Timer />)
-            expect(wrapper.state('startTime')).to.equal(null)
-        })
+    // describe('loading state & storage', ()=>{
+    //     before(function () {
+    //         global.chrome = chrome
+    //     })
+    //     it('should have inital state of startTime = null', ()=>{
+    //         const wrapper = shallow(<Timer />)
+    //         expect(wrapper.state('startTime')).to.equal(null)
+    //     })
 
-        it('should not store a start time on inital load in chrome storage', ()=>{
-           expect(chrome.storage.startTime === null || chrome.storage.startTime === undefined ) 
-        })
-    })
+    //     it('should not store a start time on inital load in chrome storage', ()=>{
+    //        expect(chrome.storage.startTime === null || chrome.storage.startTime === undefined ) 
+    //     })
+    // })
 })
 
-describe('startTime', ()=>{
+describe.only('Time functions', ()=>{
     before(function () {
         global.chrome = chrome
     })
-
-    it('returns and saves a start time in state', ()=>{
-        const wrapper = shallow(<Timer />)
-        var date = new Date()
-        expect(wrapper.instance().startTimer().getMinutes()).to.equal(date.getMinutes())
-        expect(wrapper.state('startTime').getMinutes()).to.equal(date.getMinutes())
-    })   
-})
-
-describe('Timer display', ()=>{
-    before(function () {
-        global.chrome = chrome
-    })
-
-
-
-    //TODO set up sinon to handle button clicks
-    it('displays startTime if present', ()=>{
     
+    const wrapper = shallow(<Timer />)                      
+    const date = Date.now()
+    wrapper.instance().startTimer()
+
+    it('startTime save a time in state', ()=>{
+        expect(wrapper.state('startTime')).to.equal(date)
     })
+    
+    it('calculates the time elapsed since start time', ()=>{
+        expect(wrapper.state('timeSinceStartTime')).to.equal(Date.now()- date)
+    })
+
+
 })
 
 
 
 
 
-describe('timeElapsed', ()=>{
-    it('should work out time difference to a 10ms accuracey', (done)=>{
-        const startTime = new Date()
-        setTimeout(function(){
-            const timeElapsed = getTimeElapsed(startTime)
-            expect((timeElapsed - 1000) < 10).to.be.true
-            done()
-        }, 1000)
-    })
-})
+
+
+// describe('timeElapsed', ()=>{
+//     it('should work out time difference to a 10ms accuracey', (done)=>{
+//         const startTime = new Date()
+//         setTimeout(function(){
+//             const timeElapsed = getTimeElapsed(startTime)
+//             expect((timeElapsed - 1000) < 10).to.be.true
+//             done()
+//         }, 1000)
+//     })
+// })
 
