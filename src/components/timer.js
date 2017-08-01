d7bd975946ba119131ca65df9e6b42f12c965408
Rachel Black <rachblondon@gmail.com>
2017-08-01 08:43:20 +0100

    countdown clean up, starting background menu

--- src/components/timer.js
+++ src/components/timer.js
@@ -1,17 +1,40 @@
 import React, { Component } from 'react'
-import CountDownContainer from './countDown_container.js'
+import CountDown from './countDown.js'
+import { FOCUS_TIME } from './../../utils.js'
 
 //issue with chrome 
 // fix with Date.now (maybe do to string)
 // chrome storage does not save dates objects well
 // https://bugs.chromium.org/p/chromium/issues/detail?id=161319
 
+//attach background script, figure out how to run the timers in there,
+//maybe its better to run the timers from there - although could be issues
+//with chrome.storage.get?
+
+export function inMinsAndSecs(ms){
+    const mins = Math.floor(ms / 60000)
+    const secs = (ms % 60000 / 1000).toFixed(0)/1
+    return {
+        mins : mins,
+        secs : secs
+    }
+}
 
 export default class Timer extends Component{
     
     constructor(props){
         super(props);
             this.state = {startTime: Date.now(), isTimerRunning: false}
+    }
+    
+    // if js time string is in storage pull it our
+    componentWillMount(){
+        
+    }
+
+    // if time string is in state save in storage
+    componentWillUnMount(){
+
     }
 
     startTimer(){
@@ -31,13 +54,21 @@ export default class Timer extends Component{
             this.setState({timeElapsed : Date.now() - this.state.startTime })
         }
     }
+
+    //TODO find away to handle bug where if the conter is always visable a user can 
+    //click the start  button multiple times to start in double + speed
+    // maybe remove the option to start once you have untill the intervals have been cleared
+    // also need to find a way to pause timer.
     
     render(){
+        const timers = inMinsAndSecs(FOCUS_TIME - this.state.timeElapsed)
         return <div style={{height : '100px'}}>
-  
+
+        
                     { this.state.isTimerRunning &&
-                           <CountDownContainer 
-                                timeElapsed={this.state.timeElapsed}/>
+                           <CountDown
+                                timers={timers} 
+                                />
                     }
                     {!this.state.isTimerRunning && 
                         <button className="startBtn" onClick={this.startTimer.bind(this)}> Start</button>
--- test/components/timer.spec.js
+++ test/components/timer.spec.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import chai, {expect} from 'chai'
-import Timer from '../../src/components/timer'
+import Timer, { inMinsAndSecs } from '../../src/components/timer'
 import { shallow } from 'enzyme'
 import mocha, {done } from 'mocha'
 //TODO get this working properly
@@ -8,7 +8,38 @@ import chrome from 'sinon-chrome' //https://github.com/acvetkov/sinon-chrome
 import sinon from 'sinon'
 //TODO get sinon to simulate button clicks
 
-import { TIME_TOLERANCE } from './../../utils.js'
+import { TIME_TOLERANCE, FOCUS_TIME } from './../../utils.js'
+
+
+describe('inMinsAndSecs', ()=>{
+    it('coverts 1sec correctly', ()=>{
+        expect(inMinsAndSecs(1000)).to.deep.equal({mins: 0, secs:1})
+    })
+
+    it('converts 1 min correctly', ()=>{
+        expect(inMinsAndSecs(60000)).to.deep.equal({mins: 1, secs: 0})
+    })
+
+    it('converts 4 mins 25 seconds correctly', ()=>{
+        expect(inMinsAndSecs(265000)).to.deep.equal({mins: 4, secs:25})
+    })
+
+    it('converts 25mins accurately', ()=>{
+        expect(inMinsAndSecs(1500000)).to.deep.equal({mins:25, secs: 0})
+    })
+})
+
+describe('timeleft', ()=>{
+    it('converts 1 min work completed accurately', ()=>{
+        const oneMinWorkCompleted = FOCUS_TIME - 60000
+        expect(inMinsAndSecs(oneMinWorkCompleted)).to.deep.equal({mins:24, secs: 0})
+    })
+
+    it('converts 10 mins 3 seconds work accurately',()=>{
+        const tenMins3secsWorkCompleted = FOCUS_TIME - 603000
+        expect(inMinsAndSecs(tenMins3secsWorkCompleted)).to.deep.equal({mins:14, secs: 57})
+    })
+})
 
 describe('Timer', ()=>{
     before(function () {
