109ceecd848535666d37cab99973cc182e92d02a
Rachel Black <rachblondon@gmail.com>
2017-09-05 09:49:17 +0100

    background script tidy up

--- background.js
+++ background.js
@@ -1,20 +1,4 @@
-// this file should not be in worked on in dist.
-//look at moving it using webpack ? or gulp.
 console.log("hello from background file")
-
-// chrome.extension.onMessage.addListener((request,sender,sendResponse)=>{
-//     if( request.greeting === "startTimer" ){
-//         console.log('starting timer')
-//     }
-// })
-
-// // https://developer.chrome.com/extensions/messaging
-//   var port = chrome.runtime.connect();
-//   port.onDisconnect.addListener(function() {
-//     console.log("Disconnected");
-//   });
-
-
 
 function handleMessage(request, sender, sendResponse) {
   console.log("Message from the content script: " +
@@ -23,6 +7,4 @@ function handleMessage(request, sender, sendResponse) {
 }
 
 chrome.runtime.onMessage.addListener(handleMessage);
-
-// var
 //https://stackoverflow.com/questions/2315863/does-onbeforeunload-event-trigger-for-popup-html-in-a-google-chrome-extension
