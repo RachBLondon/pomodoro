a9c79c6e33bafd4a125cb41fce279087556f8490
Rachel Black <rachblondon@gmail.com>
2017-09-11 13:26:53 +0100

    sneding time elapsed to popup

--- src/components/timer.js
+++ src/components/timer.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react'
-import CountDown from './countDown.js'
-import { FOCUS_TIME } from './../../utils.js'
+import React, { Component } from "react";
+import CountDown from "./countDown.js";
+import { FOCUS_TIME } from "./../../utils.js";
 //TODO only us sinon-chrome in test env
 // import chrome from 'sinon-chrome'
 
-//issue with chrome 
+//issue with chrome
 // fix with Date.now (maybe do to string)
 // chrome storage does not save dates objects well
 // https://bugs.chromium.org/p/chromium/issues/detail?id=161319
@@ -13,46 +13,48 @@ import { FOCUS_TIME } from './../../utils.js'
 //maybe its better to run the timers from there - although could be issues
 //with chrome.storage.get?
 
-export function inMinsAndSecs(ms){
-    const mins = Math.floor(ms / 60000)
-    const secs = (ms % 60000 / 1000).toFixed(0)/1
-    return {
-        mins : mins,
-        secs : secs
-    }
+export function inMinsAndSecs(ms) {
+  const mins = Math.floor(ms / 60000);
+  const secs = ((ms % 60000) / 1000).toFixed(0) / 1;
+  return {
+    mins: mins,
+    secs: secs
+  };
 }
 
-export default class Timer extends Component{
-    
+export default class Timer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { timeElapsed: 0 };
+  }
 
-    
+  componentWillMount() {
+    chrome.runtime.sendMessage("getTime", timeElapsed => {
+      this.setState({ timeElapsed: timeElapsed.timeElapsed }, () => {
+        console.log("after change ", this.state);
+      });
+    });
+  }
 
-    componentWillMount(){
-        chrome.runtime.sendMessage("getTime", (timeElapsed)=>{
-            console.log("hats ", timeElapsed.timeElapsed)
-            this.setState({timeElapsed: timeElapsed.timeElapsed})
+  startTimer() {
+    chrome.runtime.sendMessage("startTimer");
+  }
 
-        })
-    }
+  stopTimer() {
+    chrome.runtime.sendMessage("stopTimer");
+  }
 
-
-    startTimer(){
-        chrome.runtime.sendMessage("startTimer")
-
-    }
-
-    stopTimer(){
-        chrome.runtime.sendMessage("stopTimer")
-    }
-
-
-
-    
-    render(){
-        return <div style={{height : '100px'}}>
-                        <button className="startBtn" onClick={this.startTimer}> Start</button>             
-                        <button className="stopBtn" onClick={this.stopTimer}>Stop </button>                                       
-                </div>
-    }
+  render() {
+    return (
+      <div style={{ height: "100px" }}>
+        <button className="startBtn" onClick={this.startTimer.bind(this)}>
+          {" "}
+          Start
+        </button>
+        <button className="stopBtn" onClick={this.stopTimer}>
+          Stop{" "}
+        </button>
+      </div>
+    );
+  }
 }
-
