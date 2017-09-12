9e21032e6bc557b4a4d0f67e60cd606f49a0cbcf
Rachel Black <rachblondon@gmail.com>
2017-09-12 09:55:51 +0100

    stop functionality added

--- background.js
+++ background.js
@@ -1,41 +1,67 @@
 var state = {}
 var timer
 
-
-function startTimer(){
-  state.startTime = Date.now()
-  state.isTimerRUnning = true
-  timer = setInterval(()=>{
-     tick()
-  }, 1000)
-    console.log("state :", state)
-}
-
-function tick(){
-    console.log("tick state ", state)
-    if(state.startTime){
-        state.timeElapsed = Date.now() - state.startTime 
+var timerObj = {
+  startTimer: function () {
+    this.startTime = Date.now()
+    this.isTimerRUnning = true
+    this.timer = setInterval(() => {
+      const boundTick = timerObj.tick.bind(timerObj)
+      boundTick()
+    }, 1000)
+  },
+  tick: function () {
+    console.log("tick :", this)
+    if (this.startTime) {
+      this.timeElapsed = Date.now() - this.startTime
     }
+  },
+  stopTimer: function () {
+    clearInterval(this.timer)
+    this.startTime = null
+    this.isTimerRUnning = false
+    this.oldTimeElapsed = this.timeElapsed
+    this.timeElapsed = 0
+    console.log("stop timer", this)
+  }
 }
 
-function stopTimer(){
+// function startTimer() {
+//   state.startTime = Date.now()
+//   state.isTimerRUnning = true
+//   timer = setInterval(() => {
+//     tick()
+//   }, 1000)
+//   console.log("state :", state)
+// }
+
+// function tick() {
+//   console.log("tick state ", state)
+//   if (state.startTime) {
+//     state.timeElapsed = Date.now() - state.startTime
+//   }
+// }
+
+function stopTimer() {
   state = {}
   clearInterval(timer)
 }
 
 function handleMessage(request, sender, sendResponse) {
 
-    if(request === 'startTimer'){
-      startTimer()
-    }
+  if (request === 'startTimer') {
+    timerObj.startTimer()
+  }
 
-    if(request === 'stopTimer'){
-      stopTimer()
-    }
+  if (request === 'stopTimer') {
+    timerObj.stopTimer()
+  }
 
-    if(request === 'getTime'){
-      sendResponse({timeElapsed : state.timeElapsed})
-    }
+  if (request === 'getTime') {
+    sendResponse({
+      timeElapsed: state.timeElapsed
+    })
+  }
 }
 
 chrome.runtime.onMessage.addListener(handleMessage);
