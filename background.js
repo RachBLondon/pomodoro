bf901ef826967da0ceef275bd3c34d6a1e99af7c
Rachel Black <rachblondon@gmail.com>
2017-09-23 14:05:09 +0100

    temp commiy

--- background.js
+++ background.js
@@ -1,58 +1,11 @@
-var state = {}
-var timer
-import { startTimer } from "./utils"
+import { startTimer, tick, stopTimer } from "./utils"
 
-// var timerObj = {
-//   startTimer: function () {
-//     this.startTime = Date.now()
-//     this.isTimerRUnning = true
-//     this.timer = setInterval(() => {
-//       const boundTick = timerObj.tick.bind(timerObj)
-//       boundTick()
-//     }, 1000)
-//   },
-//   tick: function () {
-//     console.log("tick :", this)
-//     if (this.startTime) {
-//       this.timeElapsed = Date.now() - this.startTime
-//     }
-//   },
-//   stopTimer: function () {
-//     clearInterval(this.timer)
-//     this.startTime = null
-//     this.isTimerRUnning = false
-//     this.oldTimeElapsed = this.timeElapsed
-//     this.timeElapsed = 0
-//     console.log("stop timer", this)
-//   }
-// }
-
-// function startTimer() {
-//   state.startTime = Date.now()
-//   state.isTimerRUnning = true
-//   timer = setInterval(() => {
-//     tick()
-//   }, 1000)
-//   console.log("state :", state)
-// }
-
-function tick() {
-  console.log("tick state ", state)
-  if (state.startTime) {
-    state.timeElapsed = Date.now() - state.startTime
-  }
-}
-
-function stopTimer() {
-  state = {}
-  clearInterval(timer)
-}
 
 function handleMessage(request, sender, sendResponse) {
 
   if (request === 'startTimer') {
     console.log("in start timer")
-    startTimer(state, timer, tick)
+    startTimer( tick)
   }
 
   if (request === 'stopTimer') {
@@ -60,7 +13,7 @@ function handleMessage(request, sender, sendResponse) {
   }
 
   if (request === 'getTime') {
-    sendResponse(state)
+    // sendResponse()
   }
 }
 
--- utils.js
+++ utils.js
@@ -4,12 +4,29 @@ export const FOCUS_TIME = 1500000; // 25 mins
 //Testing constants
 export const TIME_TOLERANCE = 2;
 
+
+var timer
+var state = {}
 //timer functions
-export function startTimer(state, timer, tick) {
-    state.startTime = Date.now()
-    state.isTimerRunning = true
-    timer = setInterval(()=>{
-        tick()
-    }, 1000)
-    console.log("start timer utils ", state)
+export function startTimer(tick) {
+  state.startTime = Date.now();
+  state.isTimerRunning = true;
+  timer = setInterval(() => {
+    tick();
+  }, 1000);
+  console.log("start timer utils ", state);
+}
+
+export function tick() {
+  console.log("tick state ");
+  if (state.startTime) {
+    state.timeElapsed = Date.now() - state.startTime;
+  }
+}
+
+export function stopTimer() {
+  console.log("in stop timers")  
+  state = {};
+  
+  clearInterval(timer);
 }
