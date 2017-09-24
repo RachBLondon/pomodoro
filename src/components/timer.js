031975423180986b76dff1a169c26461b42da500
Rachel Black <rachblondon@gmail.com>
2017-09-24 09:46:50 +0100

    startTime and time elased in popup

--- src/components/timer.js
+++ src/components/timer.js
@@ -1,31 +1,19 @@
 import React, { Component } from "react";
 import CountDown from "./countDown.js";
 import { FOCUS_TIME } from "./../../utils.js";
+import { startTimer, tick, stopTimer, getTime, inMinsAndSecs } from "./../../utils"
+
 //TODO only us sinon-chrome in test env
 // import chrome from 'sinon-chrome'
 
-//issue with chrome
-// fix with Date.now (maybe do to string)
-// chrome storage does not save dates objects well
-// https://bugs.chromium.org/p/chromium/issues/detail?id=161319
 
-//attach background script, figure out how to run the timers in there,
-//maybe its better to run the timers from there - although could be issues
-//with chrome.storage.get?
 
-export function inMinsAndSecs(ms) {
-  const mins = Math.floor(ms / 60000);
-  const secs = ((ms % 60000) / 1000).toFixed(0) / 1;
-  return {
-    mins: mins,
-    secs: secs
-  };
-}
+
 
 export default class Timer extends Component {
   constructor(props) {
     super(props);
-    this.state = { timeElapsed: 0 };
+    this.state = { timeElapsed: 0, isTimerRunning : false };
   }
 
   componentWillMount() {
@@ -36,6 +24,7 @@ export default class Timer extends Component {
       //TODO if running
       if(state.isTimerRunning){
         console.log("is running yays")
+        this.setState({isTimerRunning : true, startTime : state.startTime, timeElapsed : state.startTime - Date.now()})
       }
 
     
@@ -55,6 +44,9 @@ export default class Timer extends Component {
   render() {
     return (
       <div style={{ height: "100px" }}>
+        {this.state.isTimerRunning ? "Timer Running" : "Not running"}
+        <p>StartTime {this.state.isTimerRunning && this.state.startTime}</p>
+        <p>TimeElasped{this.state.isTimerRunning && this.state.timeElapsed}</p>
         <button className="startBtn" onClick={this.startTimer.bind(this)}>
           {" "}
           Start
